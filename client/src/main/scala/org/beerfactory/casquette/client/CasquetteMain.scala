4a2b1db8363dfd32086e4835a87317ce5e9a7bbd
njouanin <nico@beerfactory.org>
2016-02-16 22:15:07 +0100

    Init Casquette client App

---
+++ client/src/main/scala/org/beerfactory/casquette/client/CasquetteMain.scala
@@ -0,0 +1,29 @@
+package org.beerfactory.casquette.client
+
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+/**
+  * Created by nico on 16/02/2016.
+  */
+object CasquetteMain extends App {
+  val logger = Logger(LoggerFactory.getLogger("CasquetteMain"))
+  logger.debug("Starting ...")
+
+  private lazy val config = {
+    val _config = ConfigFactory.load()
+    val internalConfig = _config.getConfig("casquette.internal-config")
+
+    _config
+      .withoutPath("akka")
+      .withFallback(internalConfig)
+  }
+
+  val system = ActorSystem("casquette-client", config)
+  logger.debug("System started")
+
+  system.terminate()
+  logger.debug("Stopped")
+}
