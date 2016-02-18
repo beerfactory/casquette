2016ef8f207e426170e17958d841051c04a1f7f8
nico <nico@beerfactory.org>
2016-02-18 11:21:46 +0100

    Refactor for test

--- core/src/main/scala/org/beerfactory/casquette/core/mqtt/packets/CodecFlow.scala
+++ core/src/main/scala/org/beerfactory/casquette/core/mqtt/packets/CodecFlow.scala
@@ -2,11 +2,15 @@ package org.beerfactory.casquette.core.mqtt.packets
 
 import akka.stream.scaladsl.BidiFlow
 import scodec._
-
+import scodec.bits.BitVector
 
 /**
   * Created by nico on 17/02/2016.
   */
 object CodecFlow {
-  //def codecFlow = BidiFlow.fromFunctions(Codec[MQTTPacket].encode, Codec[MQTTPacket].decode)
+  def decode(data: BitVector): MQTTPacket = Codec[MQTTPacket].decode(data).require.value
+
+  def encode(packet: MQTTPacket):BitVector = Codec[MQTTPacket].encode(packet).require
+
+  def codecFlow = BidiFlow.fromFunctions(decode, encode)
 }
