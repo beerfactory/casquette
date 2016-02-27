1c69d26cec238b23b2910bd7183417fdd89389f3
njouanin <nico@beerfactory.org>
2016-02-27 15:58:56 +0100

    Add getHostPort method

--- client/src/main/scala/org/beerfactory/casquette/client/MQTTClientActor.scala
+++ client/src/main/scala/org/beerfactory/casquette/client/MQTTClientActor.scala
@@ -1,10 +1,12 @@
 package org.beerfactory.casquette.client
 
+import java.net.{MalformedURLException, URI, InetSocketAddress, InetAddress}
+
 import akka.actor.{Actor, ActorLogging}
 import akka.event.LoggingReceive
 import org.beerfactory.casquette.core.CommonToolsActor
 
-import scala.util.Random
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by nico on 19/02/2016.
@@ -15,10 +17,23 @@ class MQTTClientActor extends Actor with ActorLogging {
 
   private def notConnected(): Receive = LoggingReceive {
     case Status => sender() ! Disconnected
-    case c:Connect ⇒ doConnect(c)
+    //case c:Connect ⇒ doConnect(c)
   }
 
-  private def doConnect(c: Connect) = {
-
+  private[client] def getHostPort(uri: URI): Try[(String, Int)] = {
+    if (!List("mqtt", "mqtts", "ws", "wss").contains(uri.getScheme))
+      Failure(new MalformedURLException(s"Invalid protocol $uri.getScheme()"))
+    else {
+      val port = uri.getPort match {
+        case p if p > -1 => p
+        case _ => uri.getScheme match {
+          case "mqtt" => context.system.settings.config.getInt("casquette.default.tcp-port.mqtt")
+          case "mqtts" => context.system.settings.config.getInt("casquette.default.tcp-port.mqtts")
+          case "ws" => context.system.settings.config.getInt("casquette.default.tcp-port.ws")
+          case "wss" => context.system.settings.config.getInt("casquette.default.tcp-port.wss")
+        }
+      }
+      Success[(String, Int)]((uri.getHost, port))
+    }
   }
 }
--- client/src/test/scala/org/beerfactory/casquette/client/MQTTClientActorSpec.scala
+++ client/src/test/scala/org/beerfactory/casquette/client/MQTTClientActorSpec.scala
@@ -1,11 +1,56 @@
 package org.beerfactory.casquette.client
 
+import java.net.URI
+
 import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import org.specs2.mutable.Specification
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import akka.pattern.ask
+
+import scala.util.Success
 
 /**
   * Created by nico on 22/02/2016.
   */
 class MQTTClientActorSpec extends Specification {
+  "getHostPort method" should {
+    implicit val system = ActorSystem("TestActorSystem", ConfigFactory.load())
+    implicit val timeout = Timeout(10 seconds)
+    val actorRef = TestActorRef(new MQTTClientActor())
+
+    "return ('localhost', 1883) from \"mqtt://localhost\"" in {
+      val actor = actorRef.underlyingActor
+      actor.getHostPort(new URI("mqtt://localhost")) mustEqual Success(("localhost", 1883))
+    }
+
+    "return ('localhost', 8883) from \"mqtts://localhost\"" in {
+      val actor = actorRef.underlyingActor
+      actor.getHostPort(new URI("mqtts://localhost")) mustEqual Success(("localhost", 8883))
+    }
+
+    "return ('localhost', 80) from \"ws://localhost\"" in {
+      val actor = actorRef.underlyingActor
+      actor.getHostPort(new URI("ws://localhost")) mustEqual Success(("localhost", 80))
+    }
+
+    "return ('localhost', 443) from \"wss://localhost\"" in {
+      val actor = actorRef.underlyingActor
+      actor.getHostPort(new URI("wss://localhost")) mustEqual Success(("localhost", 443))
+    }
+
+    "return ('localhost', 1884) from \"mqtt://localhost:1884\"" in {
+      val actor = actorRef.underlyingActor
+      actor.getHostPort(new URI("mqtt://localhost:1884")) mustEqual Success(("localhost", 1884))
+    }
+
+    "fail with incorrect scheme \"mttq://localhost\"" in {
+      val actor = actorRef.underlyingActor
+      actor.getHostPort(new URI("mttq://localhost")) must beFailedTry
+    }
+  }
+
 }
