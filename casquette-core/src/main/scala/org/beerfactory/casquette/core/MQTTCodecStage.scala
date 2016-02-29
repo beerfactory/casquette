9b4e7e4cc5d2bc1e193e64ba068b9b5c0cf64597
njouanin <nico@beerfactory.org>
2016-02-29 22:29:31 +0100

    Need test implementation

--- casquette-core/src/main/scala/org/beerfactory/casquette/core/MQTTCodecStage.scala
+++ casquette-core/src/main/scala/org/beerfactory/casquette/core/MQTTCodecStage.scala
@@ -3,14 +3,17 @@ package org.beerfactory.casquette.core
 import akka.stream.{Attributes, Outlet, Inlet, BidiShape}
 import akka.stream.stage.{OutHandler, InHandler, GraphStageLogic, GraphStage}
 import akka.util.ByteString
+import com.typesafe.scalalogging.LazyLogging
 import org.beerfactory.casquette.mqtt.MQTTPacket
+import scodec.Attempt.{Failure, Successful}
 import scodec.Codec
+import scodec.Err.InsufficientBits
 import scodec.bits.{ByteVector, BitVector}
 
 /**
   * Created by njouanin on 29/02/16.
   */
-class MQTTCodecStage extends GraphStage[BidiShape[ByteString, MQTTPacket, MQTTPacket, ByteString]] {
+class MQTTCodecStage extends GraphStage[BidiShape[ByteString, MQTTPacket, MQTTPacket, ByteString]] with LazyLogging {
   val in1 = Inlet[ByteString]("MQTTCodecStage.in1")
   val out1 = Outlet[MQTTPacket]("MQTTCodecStage.out1")
   val in2 = Inlet[MQTTPacket]("MQTTCodecStage.in2")
@@ -25,11 +28,14 @@ class MQTTCodecStage extends GraphStage[BidiShape[ByteString, MQTTPacket, MQTTPa
         override def onPush(): Unit = {
           val bytes = grab(in1)
           buffer = buffer ++ BitVector.view(bytes.toByteBuffer)
-          decode()
+          push_decoded(push(out1,_))
         }
         override def onUpstreamFinish(): Unit =
           {
-            ???
+            while(!buffer.isEmpty)
+            {
+              push_decoded(emit(out1,_))
+            }
             complete(out1)
           }
       })
@@ -41,22 +47,44 @@ class MQTTCodecStage extends GraphStage[BidiShape[ByteString, MQTTPacket, MQTTPa
         override def onUpstreamFinish(): Unit = complete(out2)
       })
       setHandler(out1, new OutHandler {
-        override def onPull(): Unit = ???
+        override def onPull(): Unit = {
+          if(!isClosed(in1))
+            pull(in1)
+          else
+            push_decoded(push(out1,_))
+        }
       })
       setHandler(out2, new OutHandler {
-        override def onPull(): Unit = ???
+        override def onPull(): Unit = {
+          if(!isClosed(in2))
+            pull(in2)
+        }
       })
 
-      private def decode(): Unit = ???
-
       private def push_encoded(message:MQTTPacket): Unit = {
-        val result = Codec[MQTTPacket].encode(message)
-        if(result.isSuccessful)
-          push(out2, ByteString(result.require.toByteBuffer))
-        else
-          {
-            //TODO: Report error
+        Codec[MQTTPacket].encode(message) match {
+          case Successful(value) => push(out2, ByteString(value.toByteBuffer))
+          case Failure(cause) => logger.warn(s"Error while encoding message $message: $cause")
+        }
+      }
+
+      private def push_decoded(f:(MQTTPacket) => Unit): Unit = {
+        if(!buffer.isEmpty) {
+          Codec[MQTTPacket].decode(buffer) match {
+            case Successful(result) =>
+              f(result.value)
+              buffer = result.remainder
+            case Failure(cause) => {
+              case cause: InsufficientBits ⇒
+                // Not enough bytes have been read to decode length -> pull more
+                if (isClosed(in1))
+                  complete(out1)
+                else
+                  pull(in1)
+              case _ ⇒ failStage(new MQTTFrameParserException("Failed to decoded incoming data frame", Some(cause)))
+            }
           }
+        }
       }
     }
   }
---
+++ casquette-core/src/test/scala/org/beerfactory/casquette/core/MQTTCodecStageSpec.scala
@@ -0,0 +1,34 @@
+package org.beerfactory.casquette.core
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{BidiFlow, Flow, Source}
+import akka.stream.testkit.scaladsl.TestSink
+import akka.util.{ByteString, Timeout}
+import com.typesafe.config.ConfigFactory
+import org.beerfactory.casquette.mqtt.MQTTPacket
+import org.specs2.mutable.Specification
+import scodec.bits.ByteVector
+import scala.concurrent.duration._
+
+
+/**
+  * Created by nico on 29/02/2016.
+  */
+class MQTTCodecStageSpec extends Specification {
+  "MQTT frame parser" should {
+    implicit val system = ActorSystem("test-system", ConfigFactory.load())
+    implicit val materializer = ActorMaterializer()
+    implicit val timeout = Timeout(10 seconds)
+
+    "[1] Parse MQTT packet from basic byte stream" in {
+      val packetBytes = Array[Byte](0x00, 0x01, 0x00)
+//      val sourceUnderTest = Source(List(ByteString(packetBytes))).toMat(BidiFlow.fromGraph(new MQTTCodecStage))
+//      sourceUnderTest.runWith(TestSink.probe[MQTTPacket])
+//        .request(1)
+//        .expectNext(ByteVector(packetBytes))
+//        .expectComplete() must not(throwA[AssertionError])
+    }
+
+
+  }
