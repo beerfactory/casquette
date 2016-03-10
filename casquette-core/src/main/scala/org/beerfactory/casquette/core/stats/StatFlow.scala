729afe9397ce8e709c20865b5a7a36c6d2633901
njouanin <nico@beerfactory.org>
2016-03-10 23:39:20 +0100

    Refactor stat flows (tests to be done)

--- casquette-core/src/main/scala/org/beerfactory/casquette/core/stats/StatFlow.scala
+++ casquette-core/src/main/scala/org/beerfactory/casquette/core/stats/StatFlow.scala
@@ -1,23 +1,52 @@
 package org.beerfactory.casquette.core.stats
 
-import akka.stream.{Outlet, FanOutShape, FanOutShape2, Graph}
-import akka.stream.scaladsl.{Broadcast, GraphDSL}
+import akka.actor.ActorRef
+import akka.stream.scaladsl.BidiFlow
 import akka.util.ByteString
+import org.beerfactory.casquette.mqtt.PacketType.PacketType
+import org.beerfactory.casquette.mqtt.{PacketType, MQTTPacket}
+import org.beerfactory.casquette.mqtt.PacketType.PacketType
+
 
 /**
   * Created by nico on 08/03/2016.
   */
 sealed trait Stat
-case class ByteStat(size: Int) extends Stat
+case class BytesInStat(size: Int) extends Stat
+case class BytesOutStat(size: Int) extends Stat
+case class PacketInStat(packetType: PacketType) extends Stat
+case class PacketOutStat(packetType: PacketType) extends Stat
+
 
 object StatFlow {
-  def bytesThroughStat = {
-    GraphDSL.create() { b =>
-      import GraphDSL.Implicits._
-
-      val broadcast = b.add(Broadcast[ByteString](2))
-      val statOut = Outlet[Stat]("StatFlow.out")
-      new StateShape(broadcast.in, broadcast.out(0), statOut)
+  private def bytesThroughStatFlow(statActor: ActorRef, topFlowStatBuilder:(ByteString) => Stat, bottomFlowStatBuilder:(ByteString) => Stat) = {
+    def statBytes(statActor: ActorRef, statBuilder:(ByteString) => Stat)(data: ByteString) = {
+      statActor ! statBuilder(data)
+      data
     }
+    BidiFlow.fromFunctions(
+      statBytes(statActor, topFlowStatBuilder),
+      statBytes(statActor, bottomFlowStatBuilder)
+    )
   }
+  private def packetThroughStatFlow(statActor: ActorRef, topFlowStatBuilder:(MQTTPacket) => Stat, bottomFlowStatBuilder:(MQTTPacket) => Stat) = {
+    def statPacket(statActor: ActorRef, statBuilder:(MQTTPacket) => Stat)(packet: MQTTPacket) = {
+      statActor ! statBuilder(packet)
+      packet
+    }
+    BidiFlow.fromFunctions(
+      statPacket(statActor, topFlowStatBuilder),
+      statPacket(statActor, bottomFlowStatBuilder)
+    )
+  }
+
+  def bytesThroughCounterFlow(statActor: ActorRef) = {
+    bytesThroughStatFlow(statActor, data => BytesInStat(data.length), data => BytesOutStat(data.length))
+  }
+  def packetThroughCounterFlow(statActor: ActorRef) = {
+    packetThroughStatFlow(statActor,
+      packet => PacketInStat(PacketType.fromPacket(packet)),
+      packet => PacketOutStat(PacketType.fromPacket(packet)))
+  }
+
 }
---
+++ casquette-core/src/test/scala/org/beerfactory/casquette/core/stats/StatFlowSpec.scala
@@ -0,0 +1,34 @@
+package org.beerfactory.casquette.core.stats
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.testkit.scaladsl.TestSink
+import akka.testkit.{TestProbe, TestActors}
+import akka.util.{Timeout, ByteString}
+import com.typesafe.config.ConfigFactory
+import org.specs2.mutable.Specification
+import scala.concurrent.duration._
+
+/**
+  * Created by nico on 10/03/2016.
+  */
+class StatFlowSpec extends Specification {
+  implicit val system = ActorSystem("test-system", ConfigFactory.load())
+  implicit val materializer = ActorMaterializer()
+  implicit val timeout = Timeout(10 seconds)
+
+  "Bytes through counter" should {
+    "Generate 2 bytes stat " in {
+      val probe = TestProbe()
+      val elem = ByteString("ABCD")
+      val sourceUnderTest = Source.single(elem).via(StatFlow.bytesThroughCounterFlow(probe.ref).join(Flow[ByteString].map(x => x)))
+      sourceUnderTest.runWith(TestSink.probe[ByteString])
+        .request(1)
+        .expectNext(elem)
+        .expectComplete() must not(throwA[AssertionError])
+      probe.expectMsg(BytesInStat(elem.length)) must not(throwA[AssertionError])
+      probe.expectMsg(BytesOutStat(elem.length)) must not(throwA[AssertionError])
+    }
+  }
+}
